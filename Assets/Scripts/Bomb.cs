b60e96a78b77a8630900f71d3fe77633356b9bea
Krzysztof Ciombor <krzysztofciombor@gmail.com>
2016-03-17 18:36:40 +0100

    Add bomb - WIP

---
+++ Assets/Scripts/Bomb.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bomb : MonoBehaviour {
+
+	public float detonateDelay;
+	public GameObject[] explosions;
+
+	public void PlaceBomb() {
+		StartCoroutine (Detonate());
+	}
+
+	IEnumerator Detonate() {
+		yield return new WaitForSeconds(detonateDelay);
+
+		int explosionIndex = Random.Range (0, explosions.Length);
+		GameObject explosionObject = Instantiate(explosions[explosionIndex], this.transform.position, Quaternion.identity) as GameObject;
+
+		ParticleSystem explosion = explosionObject.GetComponent<ParticleSystem> ();
+		explosion.Play ();
+		DestroyBoxes ();
+		Destroy (this.gameObject);
+		Destroy (explosionObject, explosion.duration);
+	}
+
+	private void DestroyBoxes() {
+		RaycastHit[] hitBoxes = Physics.SphereCastAll (this.transform.position, 4f, Vector3.down);
+		if (hitBoxes != null) {
+			foreach (RaycastHit hit in hitBoxes) {
+				GameObject hitGameObject = hit.collider.gameObject;
+				if (hitGameObject.tag.Equals("Destructible")) {
+					Destroy (hitGameObject);
+				}
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/BombManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BombManager : MonoBehaviour {
+
+	public Bomb bombPrefab;
+
+	void Update () {
+		if (Input.GetKeyDown ("f")) {
+			Vector3 bombPosition = this.transform.position + this.transform.forward * bombPrefab.transform.localScale.x;
+			Bomb bomb = Instantiate (bombPrefab, bombPosition, Quaternion.identity) as Bomb;
+			bomb.PlaceBomb ();
+		}
+	}
+}
