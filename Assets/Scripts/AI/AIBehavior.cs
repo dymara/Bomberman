39c977c5ccec59d6afad9de8540be9f5c2b3a82e
Krzysztof Ciombor <krzysztofciombor@gmail.com>
2016-04-24 12:08:31 +0200

    Add base class for AIBehavior

---
+++ Assets/Scripts/AI/AIBehavior.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using Assets.Scripts.Board;
+using Assets.Scripts.Util;
+
+public abstract class AIBehavior : MonoBehaviour
+{
+
+    protected Board board;
+
+    protected PositionConverter positionConverter;
+
+    // Use this for initialization
+    public virtual void Start()
+    {
+        board = GameObject.Find(Constants.GAME_MANAGER_NAME)
+            .GetComponent<GameManager>().GetBoard();
+        positionConverter = GameObject.Find(Constants.GAME_MANAGER_NAME)
+            .GetComponent<GameManager>().GetPositionConverter();
+        Move();
+    }
+
+    protected abstract GameCell GetNextMove();
+
+    private void Move()
+    {
+        GameCell nextCell = GetNextMove();
+        MakeMove(nextCell);
+    }
+
+    void MakeMove(GameCell targetCell)
+    {
+        iTween.MoveTo(this.gameObject, iTween.Hash(
+            "position", positionConverter.ConvertBoardPositionToScene(targetCell.GetCoordinates(), true),
+            "oncomplete", "Move",
+            "time", 3,
+            "easetype", "linear"));
+    }
+}
--- Assets/Scripts/AI/RandomAI.cs
+++ Assets/Scripts/AI/RandomAI.cs
@@ -1,47 +1,20 @@
 ﻿using UnityEngine;
-using System.Collections;
 using Assets.Scripts.Board;
-using Assets.Scripts.Util;
 using System.Collections.Generic;
 
-public class RandomAI : MonoBehaviour {
-	
-	Board board;
-	
-	PositionConverter positionConverter;
-	
-	private bool isMoving;
+public class RandomAI : AIBehavior
+{
 
-	// Use this for initialization
-	void Start () {
-		board = GameObject.Find(Constants.GAME_MANAGER_NAME)
-			.GetComponent<GameManager>().GetBoard();
-		positionConverter = GameObject.Find(Constants.GAME_MANAGER_NAME)
-			.GetComponent<GameManager>().GetPositionConverter();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if (!isMoving) {
-			isMoving = true;
-			Move();
-		}
-	}
-	
-	void Move() 
-	{
-		Vector2 currentPosition = positionConverter.ConvertScenePositionToBoard(this.transform.position);
-		List<GameCell> adjacentCells = board.GetAdjacentCells(currentPosition);
-		GameCell nextCell = adjacentCells[Random.Range(0, adjacentCells.Count)];
-		MakeMove(nextCell);
-	}
-	
-	void MakeMove(GameCell targetCell) 
-	{
-		iTween.MoveTo(this.gameObject, iTween.Hash(
-			"position", positionConverter.ConvertBoardPositionToScene(targetCell.GetCoordinates(), true),
-			"oncomplete", "Move",
-			"time", 3,
-			"easetype", "linear"));	
-	}
+    public override void Start()
+    {
+        base.Start();
+    }
+
+    override protected GameCell GetNextMove()
+    {
+        Vector2 currentPosition = positionConverter.ConvertScenePositionToBoard(this.transform.position);
+        List<GameCell> adjacentCells = board.GetAdjacentCells(currentPosition);
+        GameCell nextCell = adjacentCells[Random.Range(0, adjacentCells.Count)];
+        return nextCell;
+    }
 }
