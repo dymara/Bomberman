a9b3789a7c106049c774733451d8347268f03109
Mariusz Brudny <mariusz.brudny@gmail.com>
2016-05-04 16:16:49 +0200

    Interaction with exit added.

--- Assets/Scripts/Model/Player.cs
+++ Assets/Scripts/Model/Player.cs
@@ -1,10 +1,15 @@
 ﻿using System;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace Assets.Scripts.Model
 {
     public class Player : AbstractPlayer
     {
         private long score;
+
+        private Boolean exitReached = false;
 
         public Player(String name, int lives) : base(lives)
         {
@@ -20,6 +25,34 @@ namespace Assets.Scripts.Model
         public void AddToScoreLong(long bonus)
         {
             score += bonus;
+        }
+
+
+        private void OnControllerColliderHit(ControllerColliderHit hit)
+        {
+
+            Exit exit = hit.gameObject.GetComponent<Exit>();
+            if (!exitReached && exit != null)
+            {
+                OnExitReached();
+            }
+        }
+
+        private void OnExitReached()
+        {
+            exitReached = true;
+            Debug.Log(DateTime.Now + " Maze exit reached");
+            Boolean answer = EditorUtility.DisplayDialog("Bomberman3D", "Congratulation!. You win.", "Next level", "Exit");
+            if (answer)
+            {
+                Debug.Log(DateTime.Now + " Loading next level");
+                SceneManager.LoadScene("Main");
+            }
+            else {
+                Debug.Log("EXIT GAME");
+                Application.Quit();
+                exitReached = false;
+            }
         }
     }
 }
