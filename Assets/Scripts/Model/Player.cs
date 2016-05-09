b80457b7f3e4061a66f42ea6069447823ae45cfe
Mariusz Brudny <mariusz.brudny@gmail.com>
2016-05-09 17:41:30 +0200

    [NT] Exit reaching improved. Bug with ending game when exit is hidden fixed.

--- Assets/Scripts/Model/Player.cs
+++ Assets/Scripts/Model/Player.cs
@@ -43,7 +43,6 @@ namespace Assets.Scripts.Model
             }
         }
 
-
         protected override void Kill()
         {
             FirstPersonController controller = gameObject.GetComponent<FirstPersonController>();
