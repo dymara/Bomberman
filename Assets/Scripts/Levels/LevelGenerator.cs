681d4c2ecfae3e72aac53c53ee7eda68165ca2cb
Krzysztof Ciombor <krzysztofciombor@gmail.com>
2016-05-27 10:48:20 +0200

    Add level generator config

--- Assets/Scripts/Levels/LevelGenerator.cs
+++ Assets/Scripts/Levels/LevelGenerator.cs
@@ -3,13 +3,6 @@ using System.Collections;
 
 public class LevelGenerator : MonoBehaviour
 {
-    public Configurator configurator;
-
-    void Awake()
-    {
-        configurator = GameManager.instance.configurator;
-    }
-
     public LevelConfig GenerateLevelConfig(int level)
     {
         LevelConfig levelConfig = new LevelConfig();
@@ -19,34 +12,39 @@ public class LevelGenerator : MonoBehaviour
         return levelConfig;
     }
 
+    /**
+    * Returns Vector2 with board dimensions x, y in number od indestructible cubes
+    * Every LEVELS_TO_INCREASE_BOARD_SIZE levels we increase the level size by 1
+    */
     private Vector2 GetBoardSize(int level)
     {
-        int sizeX = configurator.level1CubesXCount + level / 2;
-        int sizeZ = configurator.level1CubesZCount + level / 2;
+        int sizeX = LevelGeneratorConfig.LEVEL_1_BOARD_SIZE + level / LevelGeneratorConfig.LEVELS_TO_INCREASE_BOARD_SIZE;
+        int sizeZ = LevelGeneratorConfig.LEVEL_1_BOARD_SIZE + level / LevelGeneratorConfig.LEVELS_TO_INCREASE_BOARD_SIZE;
 
         return new Vector2(sizeX, sizeZ);
     }
 
     private int GetMonstersCount(int level)
     {
-        int monstersCount = configurator.level1EnemiesCount;
-        while (level-- > 0)
+        int monstersCount = level;
+        float additionalMonsterChance = 0.7f;
+        int i = level;
+        while (i-- > 0)
         {
-            if (Random.value < Mathf.Pow(0.7f, level))
+            if (Random.value < additionalMonsterChance)
             {
                 monstersCount++;
-            }
-            else
-            {
-                break;
+                additionalMonsterChance = additionalMonsterChance * 0.7f;
             }
         }
-        Debug.Log("Enemies count:" + monstersCount);
+        monstersCount = Mathf.Max(monstersCount, level);
+        Debug.Log("Level: " + level + " Enemies count:" + monstersCount);
         return monstersCount;
     }
 
     private float GetLevelDuration(Vector2 levelSize, int level)
     {
-        return levelSize.x * levelSize.y * Mathf.Max(Mathf.Pow(0.97f, level), 0.6f) * configurator.levelDurationPerBlock;
+        float levelDimen = levelSize.x * levelSize.y;
+        return levelDimen * Mathf.Max(Mathf.Pow(0.98f, level) * LevelGeneratorConfig.BASE_LEVEL_DURATION_PER_BLOCK, LevelGeneratorConfig.MIN_LEVEL_DURATION_PER_BLOCK);
     }
 }
---
+++ Assets/Scripts/Levels/LevelGeneratorConfig.cs
@@ -0,0 +1,12 @@
+﻿public class LevelGeneratorConfig
+{
+	public const int LEVEL_1_BOARD_SIZE = 3;
+	
+	public const int LEVEL_1_MONSTERS_COUNT = 1;
+	
+	public const int LEVELS_TO_INCREASE_BOARD_SIZE = 2;
+	
+	public const int BASE_LEVEL_DURATION_PER_BLOCK = 8;
+	
+	public const float MIN_LEVEL_DURATION_PER_BLOCK = 5f;
+}
