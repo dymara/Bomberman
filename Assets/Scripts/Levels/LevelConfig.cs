7350d8ba7d25ec3245585c1fca5c1597b3de71ed
Krzysztof Ciombor <krzysztofciombor@gmail.com>
2016-06-04 16:26:20 +0200

    Refomat LevelConfig.cs file

--- Assets/Scripts/Levels/LevelConfig.cs
+++ Assets/Scripts/Levels/LevelConfig.cs
@@ -8,28 +8,28 @@ public class LevelConfig
     public int monstersCount;
 
     public int findingExtraBombsCount;
-	
-	public int findingExtraLivesCount;
-	
-	public int findingBombRangeCount;
-	
-	public int findingFasterMovingCount;
-	
-	public int findingRemoteDetonationCount;
-	
+
+    public int findingExtraLivesCount;
+
+    public int findingBombRangeCount;
+
+    public int findingFasterMovingCount;
+
+    public int findingRemoteDetonationCount;
+
     public Vector2 boardSize;
 
     public override string ToString()
     {
-		string SEPARATOR = ", ";
-		return "LevelConfig {" +
-			"levelDuration: " + levelDuration + SEPARATOR +
-			"monstersCount: " + monstersCount + SEPARATOR +
-			"findingExtraLivesCount: " + findingExtraLivesCount + SEPARATOR +
-			"findingExtraBombsCount: " + findingExtraBombsCount + SEPARATOR +
-			"findingBombRangeCount: " + findingBombRangeCount + SEPARATOR +
-			"findingFasterMovingCount: " + findingFasterMovingCount + SEPARATOR +
-			"findingRemoteDetonationCount: " + findingRemoteDetonationCount + SEPARATOR + 
-			"boardSize: " + boardSize + "}";
+        string SEPARATOR = ", ";
+        return "LevelConfig {" +
+            "levelDuration: " + levelDuration + SEPARATOR +
+            "monstersCount: " + monstersCount + SEPARATOR +
+            "findingExtraLivesCount: " + findingExtraLivesCount + SEPARATOR +
+            "findingExtraBombsCount: " + findingExtraBombsCount + SEPARATOR +
+            "findingBombRangeCount: " + findingBombRangeCount + SEPARATOR +
+            "findingFasterMovingCount: " + findingFasterMovingCount + SEPARATOR +
+            "findingRemoteDetonationCount: " + findingRemoteDetonationCount + SEPARATOR +
+            "boardSize: " + boardSize + "}";
     }
 }
