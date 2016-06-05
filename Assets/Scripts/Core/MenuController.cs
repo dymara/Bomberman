e79afc0086ef57d5fa0c1caa545d5698b081032a
Andrzej Dymara <andrzejdymara@gmail.com>
2016-06-05 13:40:44 +0200

    Added handlers for main menu buttons.

--- Assets/Scripts/Core/MenuController.cs
+++ Assets/Scripts/Core/MenuController.cs
@@ -1,17 +1,34 @@
 ﻿using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityStandardAssets.Characters.FirstPerson;
 
 public class MenuController : MonoBehaviour {
 
+    private Button newGameButton;
+
+    private Button scoreBoardButton;
+
+    private Button exitButton;
+
+    private Canvas dialogCanvas;
+
+    private Button exitYesButton;
+
+    private Button exitNoButton;
+
 	// Use this for initialization
 	void Start () {
-        // TODO - THIS IS A TEMPORARY SOLUTION BEFORE A MAIN MENU SCENE GETS IMPLEMENTED
-        // GameManager.instance.ResetPlayerState();
-        // GameManager.instance.SwitchGameState(GameState.GAMEPLAY);
-
         GameManager.instance.SetCameraMenuMode(true);
-        StartCoroutine(StartGame());
+
+        this.newGameButton = GameObject.Find("Play Button").GetComponent<Button>();
+        this.scoreBoardButton = GameObject.Find("Score Board Button").GetComponent<Button>();
+        this.exitButton = GameObject.Find("Exit Button").GetComponent<Button>();
+        this.dialogCanvas = GameObject.Find("Dialog Canvas").GetComponent<Canvas>();
+        this.exitYesButton = GameObject.Find("Yes Button").GetComponent<Button>();
+        this.exitNoButton = GameObject.Find("No Button").GetComponent<Button>();
+
+        this.dialogCanvas.enabled = false;
     }
 	
 	// Update is called once per frame
@@ -19,10 +36,37 @@ public class MenuController : MonoBehaviour {
 
     }
 
-    private IEnumerator StartGame()
+    public void NewGameButtonPressed()
     {
-        yield return new WaitForSeconds(15);
         GameManager.instance.ResetPlayerState();
         GameManager.instance.SwitchGameState(GameState.GAMEPLAY);
     }
+
+    public void ScoreBoardButtonPressed()
+    {
+        GameManager.instance.SwitchGameState(GameState.SCORE_BOARD);
+    }
+
+    public void ExitButtonPressed()
+    {
+        newGameButton.interactable = false;
+        scoreBoardButton.interactable = false;
+        exitButton.interactable = false;
+        dialogCanvas.enabled = true;
+    }
+
+    public void ExitYesButtonPressed()
+    {
+        Application.Quit();
+        ExitNoButtonPressed();
+    }
+
+    public void ExitNoButtonPressed()
+    {
+        dialogCanvas.enabled = false;
+        newGameButton.interactable = true;
+        scoreBoardButton.interactable = true;
+        exitButton.interactable = true;
+    }
+
 }
