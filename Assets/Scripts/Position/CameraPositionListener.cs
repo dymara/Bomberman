6107d05e5ba2697dcf53fb39bbb1d9cba843eb3f
Andrzej Dymara <andrzejdymara@gmail.com>
2016-06-06 00:37:13 +0200

    Minimap enlarged and zoomed out.

--- Assets/Scripts/Position/CameraPositionListener.cs
+++ Assets/Scripts/Position/CameraPositionListener.cs
@@ -1,16 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace Assets.Scripts.Position
 {
     public class CameraPositionListener : IPositionListener
     {
-        private static float X_OFFSET = 9.75f;
+        private static float X_OFFSET = 13.47f;
 
-        private static float Z_OFFSET = 4.75f;
+        private static float Z_OFFSET = 7.72f;
 
         private Camera minimapCamera;
 
@@ -30,17 +26,17 @@ namespace Assets.Scripts.Position
             Vector3 cameraPostion = minimapCamera.transform.position;
             float newX = newPostion.x;
             float newZ = newPostion.z;
-            if(newX < X_OFFSET)
+            if (newX < X_OFFSET)
             {
                 newX = X_OFFSET;
-            }else if(newX > mazeWidth - X_OFFSET)
+            } else if(newX > mazeWidth - X_OFFSET)
             {
                 newX = mazeWidth - X_OFFSET;
             }
-            if(newZ < Z_OFFSET)
+            if (newZ < Z_OFFSET)
             {
                 newZ = Z_OFFSET;
-            }else if (newZ > mazeLength - Z_OFFSET)
+            } else if (newZ > mazeLength - Z_OFFSET)
             {
                 newZ = mazeLength - Z_OFFSET;
             }
